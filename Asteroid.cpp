9c8cd6a1493b50df76595873c16538ac8c6bc2e1
Ram Kotta <ramkotta@cs.utexas.edu>
2015-02-25 11:22:52 -0600

    created Asteroid.cpp and Asteroid.h

---
+++ Asteroid.cpp
@@ -0,0 +1,36 @@
+#include "Asteroid.h"
+
+//---------------------------------------------------------------------------
+Asteroid::Asteroid(Ogre::SceneManager* sceneMgr)
+{
+	mSceneMgr = sceneMgr;
+	srand(time(NULL));
+  	float minV = 1;
+  	float maxV = 3;
+  	Ogre::Real zV = minV + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(maxV-minV)));
+  	Ogre::Real xV = minV + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(maxV-minV)));
+  	asteroidVelocity = Ogre::Vector3(xV,0,zV);
+  	float minP = -650;
+  	float maxP = 650;
+  	Ogre::Real xP = minP + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(maxP-minP)));
+  	asteroidPosition = Ogre::Vector3(xP,0,minP);
+}
+//---------------------------------------------------------------------------
+Asteroid::~Asteroid(void)
+{
+}
+//---------------------------------------------------------------------------
+void Asteroid::createScene(void){
+	entAsteroid = mSceneMgr->createEntity("asteroidEntity", "sphere.mesh");
+    entAsteroid->setCastShadows(true);
+    asteroidNode = mSceneMgr->getRootSceneNode()->createChildSceneNode("asteroidNode", asteroidPosition);
+    asteroidNode->attachObject(entAsteroid);
+    asteroidNode->setScale(Ogre::Vector3(0.5f,0.5f,0.5f));
+    entAsteroid->setMaterialName("Examples/Rockwall");
+}
+//---------------------------------------------------------------------------
+void Asteroid::update(const Ogre::FrameEvent &evt){
+	asteroidNode->translate(asteroidVelocity);
+	GameObject::update(evt);
+}
+//---------------------------------------------------------------------------
---
+++ Asteroid.h
@@ -0,0 +1,29 @@
+#ifndef __Asteroid_h_
+#define __Asteroid_h_
+
+#include "GameObject.h"
+#include <Ogre.h>
+
+//---------------------------------------------------------------------------
+
+class Asteroid: public GameObject 
+{
+public:
+	Asteroid(Ogre::SceneManager* sceneMgr);
+	virtual ~Asteroid(void);
+	void createScene(void);
+	void update(const Ogre::FrameEvent &evt);
+
+protected:
+	Ogre::SceneManager* mSceneMgr;
+	Ogre::Entity *entAsteroid;
+	Ogre::SceneNode *asteroidNode;
+	Ogre::Vector3 asteroidVelocity;
+	Ogre::Vector3 asteroidPosition;
+};
+
+//---------------------------------------------------------------------------
+
+#endif // #ifndef __Asteroid_h_
+
+//---------------------------------------------------------------------------
