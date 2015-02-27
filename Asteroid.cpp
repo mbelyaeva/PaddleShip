0baef993453bd7952189e818be654a18c92e4d22
Ram Kotta <ramkotta@cs.utexas.edu>
2015-02-27 14:27:31 -0600

    Modified asteroids so that they respawn and stop floating to the right only and reduced speed of x velocity

--- Asteroid.cpp
+++ Asteroid.cpp
@@ -7,6 +7,8 @@ Asteroid::Asteroid(Ogre::SceneManager* sceneMgr) : GameObject(sceneMgr)
   float minV = 1;
   float maxV = 3;
   Ogre::Real zV = minV + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(maxV-minV)));
+  minV = -3;
+  maxV = 3;
   Ogre::Real xV = minV + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(maxV-minV)));
   asteroidVelocity = Ogre::Vector3(xV,0,zV);
   float minP = -650;
@@ -37,24 +39,24 @@ void Asteroid::addToScene(int i){
 }
 //---------------------------------------------------------------------------
 void Asteroid::update(void){
-  int outOfBounds = 0;
-  if (asteroidPosition.x >= 750 || asteroidPosition.y >= 750 || asteroidPosition.z >= 750 || asteroidPosition.x <= -750 || asteroidPosition.y <= -750 || asteroidPosition.z <= -750) {
-    outOfBounds = 1;
-  }
 
-  if (outOfBounds) {
+  if (asteroidPosition.x >= 750 || asteroidPosition.y >= 750 || asteroidPosition.z >= 750 || asteroidPosition.x <= -750 || asteroidPosition.y <= -750 || asteroidPosition.z <= -750) {
     float minV = 1;
     float maxV = 3;
     Ogre::Real zV = minV + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(maxV-minV)));
+    minV = -1;
+    maxV = 1;
     Ogre::Real xV = minV + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(maxV-minV)));
     asteroidVelocity = Ogre::Vector3(xV,0,zV);
     float minP = -650;
     float maxP = 650;
     Ogre::Real xP = minP + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(maxP-minP)));
     asteroidPosition = Ogre::Vector3(xP,0,minP);
+    asteroidNode->setPosition(asteroidPosition);
   }
 
 	asteroidNode->translate(asteroidVelocity);
+  asteroidPosition = asteroidNode->getPosition();
 	GameObject::update();
 }
 //---------------------------------------------------------------------------
